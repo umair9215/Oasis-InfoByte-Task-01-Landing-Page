0ce64e06b9d30d967be85439dfe4750478a4ffd5
Malik Muhammad Umair <167450073+umair9215@users.noreply.github.com>
2024-09-05 15:10:09 -0700

    BELLA CHIAO LANDING PAGE

--- script.js
+++ script.js
@@ -24,4 +24,29 @@ window.onscroll = () => {
 
   navbar.classList.remove("active");
   toggle.classList.remove("fa-x");
+
+
+
+
+  sections.forEach( sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height) {
+        navLinks.forEach( links => {
+            links.classList.remove('active');
+            document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active');
+        });
+    };
+});
+
+
+
 };
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav ul li a');
+
